e42dcfc1916046ad38d77a9db589f524d031e512
Louis Tagg <louist7@gmail.com>
2022-09-21 09:35:51 +0100

    add spelling request/response

---
+++ examples/example-spellings.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__ . "/../vendor/autoload.php";
+
+use Symfony\Component\Dotenv\Dotenv;
+use Silktide\ProspectClient\ProspectClient;
+
+$envFile = __DIR__ . "/../.env";
+if (file_exists($envFile)) {
+    (new Dotenv())->load($envFile);
+}
+$apiKey = $_ENV["PROSPECT_API_KEY"] ?? null;
+
+if (!is_string($apiKey)) {
+    throw new \Exception("An API key should be specified in the ./env file to run the examples");
+}
+
+$prospectClient = ProspectClient::createFromApiKey($apiKey);
+$reportApi = $prospectClient->getReportApi();
+
+$reportId = "e69ef2c48be24356a27ff77f5d6bf5ce1678e239";
+
+// Do the example
---
+++ src/Request/ReportSpellingsRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Silktide\ProspectClient\Request;
+
+use Silktide\ProspectClient\Exception\Api\ReportNotFoundException;
+use Silktide\ProspectClient\Http\HttpWrapper;
+use Silktide\ProspectClient\Response\ReportSpellingsResponse;
+
+class ReportSpellingsRequest extends AbstractRequest
+{
+    protected string $path = 'spelling-dictionary';
+    protected string $reportId;
+
+    public function __construct(HttpWrapper $httpWrapper, string $reportId)
+    {
+        parent::__construct($httpWrapper);
+        $this->reportId = $reportId;
+    }
+
+    public function addWord(string $word, string $language = 'en_GB', $caseSensitive = false): self
+    {
+        $this->method = 'POST';
+
+        $this->body['reportId'] = $this->reportId;
+        $this->body['level'] = 'report';
+        $this->body['word'] = $word;
+        $this->body['language'] = $language;
+        $this->body['caseSensitive'] = $caseSensitive;
+
+        return $this;
+    }
+
+    public function deleteWord(int $id): self
+    {
+        $this->method = 'DELETE';
+
+        $this->body['id'] = $id;
+
+        return $this;
+    }
+
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    public function execute(): ReportSpellingsResponse
+    {
+        $httpResponse = $this->httpWrapper->execute($this);
+
+        if ($httpResponse->getStatusCode() === 404) {
+            throw new ReportNotFoundException($response['error_message'] ?? 'Report not found');
+        }
+
+        return new ReportSpellingsResponse($httpResponse->getResponse());
+    }
+}
